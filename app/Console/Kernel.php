c63135783f7086395c9f5e244f3398d902bf6493
liujuguang <liujuguang@dingfudata.com>
2017-05-09 09:56:14 +0800

    bugfix: remove redundant code.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,8 +15,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        UnzipFolder::class,
-        UnzipFolderTemp::class
+        UnzipFolder::class
     ];
 
     /**
